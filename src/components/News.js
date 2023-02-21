bef885e037536a06be9f588a845d558b0907f538
Alanmp1857 <alan.panicker-bvcoel@bvp.edu.in>
2023-02-22 00:08:18 +0530

    6th commit

--- src/components/News.js
+++ src/components/News.js
@@ -26,10 +26,9 @@ export class News extends Component {
     };
   }
 
-  async componentDidMount() {
-    console.log("cdm");
-    let url =
-      `https://newsapi.org/v2/top-headlines?country=${this.props.country}&category=${this.props.category}&apiKey=d4fe1891157c44f0a0596835c10690f8&page=1&pageSize=${this.props.pageSize}`;
+  async updateNews() {
+    const url =
+      `https://newsapi.org/v2/top-headlines?country=${this.props.country}&category=${this.props.category}&apiKey=d4fe1891157c44f0a0596835c10690f8&page=${this.state.page}&pageSize=${this.props.pageSize}`;
     this.setState({ loading: true })
     let data = await fetch(url);
     let parsedData = await data.json();
@@ -40,28 +39,28 @@ export class News extends Component {
     });
   }
 
+  async componentDidMount() {
+    // let url =
+    //   `https://newsapi.org/v2/top-headlines?country=${this.props.country}&category=${this.props.category}&apiKey=d4fe1891157c44f0a0596835c10690f8&page=1&pageSize=${this.props.pageSize}`;
+    // this.setState({ loading: true })
+    // let data = await fetch(url);
+    // let parsedData = await data.json();
+    // console.log(parsedData);
+    // this.setState({
+    //   articles: parsedData.articles,
+    //   totalResults: parsedData.totalResults, loading: false
+    // });
+    this.updateNews()
+  }
+
   handlePrevClick = async () => {
-    console.log("Previous");
-    let url = `https://newsapi.org/v2/top-headlines?country=${this.props.country}&category=${this.props.category}&apiKey=d4fe1891157c44f0a0596835c10690f8&page=${this.state.page - 1
-      }&pageSize=${this.props.pageSize}`;
-    this.setState({ loading: true })
-    let data = await fetch(url);
-    let parsedData = await data.json();
-    console.log(parsedData);
-    this.setState({ page: this.state.page - 1, articles: parsedData.articles, loading: false });
+    this.setState({ page: this.state.page - 1 })
+    this.updateNews()
   };
 
   handleNextClick = async () => {
-    console.log("Next");
-    if (!(this.state.page + 1 > Math.ceil(this.state.totalResults / this.props.pageSize))) {
-      let url = `https://newsapi.org/v2/top-headlines?country=${this.props.country}&category=${this.props.category}&apiKey=d4fe1891157c44f0a0596835c10690f8&page=${this.state.page + 1
-        }&pageSize=${this.props.pageSize}`;
-      this.setState({ loading: true })
-      let data = await fetch(url);
-      let parsedData = await data.json();
-      console.log(parsedData);
-      this.setState({ page: this.state.page + 1, articles: parsedData.articles, loading: false });
-    }
+    this.setState({ page: this.state.page + 1 })
+    this.updateNews()
   };
 
   render() {
@@ -79,7 +78,7 @@ export class News extends Component {
                     element.description ? element.description.slice(0, 88) : ""
                   }
                   imageUrl={element.urlToImage}
-                  newsUrl={element.url}
+                  newsUrl={element.url} author={element.author} date={element.publishedAt} source={element.source.name}
                 />
               </div>
             );
--- src/components/NewsItem.js
+++ src/components/NewsItem.js
@@ -2,29 +2,22 @@ import React, { Component } from "react";
 
 export class NewsItem extends Component {
   render() {
-    let { title, description, imageUrl, newsUrl } = this.props;
+    let { title, description, imageUrl, newsUrl, author, date, source } = this.props;
     return (
       <div className="my-3">
         <div className="card">
           <img
-            src={
-              !imageUrl
-                ? "https://s3.cointelegraph.com/uploads/2023-02/6b8ef98c-fc1a-42bd-b7ed-b4a6e8f67eb1.jpg"
-                : imageUrl
-            }
+            src={!imageUrl ? "https://s3.cointelegraph.com/uploads/2023-02/6b8ef98c-fc1a-42bd-b7ed-b4a6e8f67eb1.jpg" : imageUrl}
             className="card-img-top"
             alt="..."
           />
           <div className="card-body">
-            <h5 className="card-title">{title}...</h5>
+            <h5 className="card-title">{title}...<span class="position-absolute top-0 translate-middle badge rounded-pill bg-danger" style={{ left: '86%', zIndex: 1 }}>
+              {source}
+            </span></h5>
             <p className="card-text">{description}...</p>
-            <a
-              href={newsUrl}
-              target={"_blank"}
-              className="btn btn-sm btn-dark"
-              rel="noreferrer">
-              Read More
-            </a>
+            <p className="card-text"><small className="text-muted">By {!author ? "Unknown" : author} on {new Date(date).toGMTString()}</small></p>
+            <a href={newsUrl} target={"_blank"} className="btn btn-sm btn-dark" rel="noreferrer">Read More</a>
           </div>
         </div>
       </div>
