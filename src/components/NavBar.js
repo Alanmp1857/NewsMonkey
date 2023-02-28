ec7dc0af7897c57e3a5ebdfae0e1e7c5da51bba2
Alanmp1857 <alan.panicker-bvcoel@bvp.edu.in>
2023-02-28 17:18:20 +0530

    some changes

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,29 +1,21 @@
-import React from "react";
+import React from 'react'
 import { Link } from "react-router-dom";
 
 const NavBar = () => {
+
   return (
     <div>
-      <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
+      <nav className="navbar fixed-top navbar-expand-lg navbar-dark bg-dark">
         <div className="container-fluid">
-          <Link className="navbar-brand" to="/">
-            NewsMonkey
-          </Link>
-          <button
-            className="navbar-toggler"
-            type="button"
-            data-bs-toggle="collapse"
-            data-bs-target="#navbarSupportedContent"
-            aria-controls="navbarSupportedContent"
-            aria-expanded="false"
-            aria-label="Toggle navigation">
+          <Link className="navbar-brand" to="/">NewsMonkey</Link>
+          <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
             <span className="navbar-toggler-icon"></span>
           </button>
-          <div
-            className="collapse navbar-collapse"
-            id="navbarSupportedContent">
+          <div className="collapse navbar-collapse" id="navbarSupportedContent">
             <ul className="navbar-nav me-auto mb-2 mb-lg-0">
-              <li className="nav-item"><Link className="nav-link active" aria-current="page" to="/general">Home</Link></li>
+              <li className="nav-item">
+                <Link className="nav-link" aria-current="page" to="/">Home</Link>
+              </li>
               <li className="nav-item"><Link className="nav-link" to="/business">Business</Link></li>
               <li className="nav-item"><Link className="nav-link" to="/entertainment">Entertainment</Link></li>
               <li className="nav-item"><Link className="nav-link" to="/general">General</Link></li>
@@ -36,7 +28,8 @@ const NavBar = () => {
         </div>
       </nav>
     </div>
-  );
+  )
+
 }
 
-export default NavBar;
+export default NavBar
