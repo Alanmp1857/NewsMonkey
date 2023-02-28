1c65bf002935cfeaefaf536c046ec8241b19acaa
Alanmp1857 <alan.panicker-bvcoel@bvp.edu.in>
2023-02-28 11:57:27 +0530

    some changes

--- src/components/Spinner.js
+++ src/components/Spinner.js
@@ -1,14 +1,12 @@
-import React, { Component } from 'react'
+import React from 'react'
 import Snake from './Snake.gif'
 
-export class Spinner extends Component {
-    render() {
-        return (
-            <div className='text-center'>
-                <img className='my-3' src={Snake} alt="loading" />
-            </div>
-        )
-    }
+const Spinner = () => {
+    return (
+        <div className='text-center'>
+            <img className='my-3' src={Snake} alt="loading" />
+        </div>
+    )
 }
 
 export default Spinner
